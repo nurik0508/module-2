2cfc401dcadcedf67df1be81d4eb14ba0bcc07b5
acernitro5nn <nursultan17ru@gmail.com>
2023-03-30 18:41:09 +0600

    first

---
+++ Lesson3/index.js
@@ -0,0 +1,92 @@
+// let name = 'Привет я Нурик...'
+// alert(name) // диалоговое окно
+
+// let howAre = prompt('Как твои дела ?', 'Не ответил') // диалоговое окно с вопросом
+
+// confirm('Ты знаешь JavaScript ?')
+// console.log(confirm('Ты знаешь JavaScript ?'))
+// console.log(!5 > 3)
+
+// let test = false || true // true
+
+// let test2 = false && true // false
+
+// let weather = prompt('Какая погода ?')
+
+// if (weather == 'Хорошая' || weather == "Отличная") {
+//   alert('Круто')
+// } else {
+//   alert('Печалька')
+// }
+
+//Home Work
+//1. let name = 'Нурсултан'
+// console.log(name)
+
+//2. let age = 29
+// console.log(age)
+
+//3. let isAdult = age
+// if (isAdult >= 18) {
+//   alert('true')
+// } else {
+//   alert('false')
+// }
+
+//4. let name = prompt("Введите ваше имя:");
+// if (name != null) {
+//   alert("Здравствуйте, " + name + "!");
+// }
+
+// 5. let userAgreed = confirm("Согласны ли вы получать рекламные материалы на вашу электронную почту?");
+
+// if (userAgreed) {
+//   console.log("Пользователь согласен на получение рекламных материалов");
+// } else {
+//   console.log("Пользователь не согласен на получение рекламных материалов");
+// }
+
+// 6. let password = "qwerty123";
+
+// if (password === "qwerty123") {
+//   console.log("Доступ разрешен");
+// } else {
+//   console.log("Доступ запрещен");
+// }
+
+// 7. let number = 6;
+
+// if (number % 2 == 0) {
+//   console.log("Число четное");
+// } else {
+//   console.log("Число нечетное");
+// }
+
+// 8. let color = "красный";
+
+// if (color === "красный") {
+//   console.log("Цвет красный");
+// } else {
+//   console.log("Цвет не красный");
+// }
+
+// 9.  let number = prompt("Введите число, которое нужно возвести в квадрат:");
+// number = Number(number);
+// let result = number * number;
+// console.log("Результат возведения числа в квадрат: " + result);
+
+// 10. let grade = "B";
+
+// if (grade == "A") {
+//   console.log("Отлично!");
+// } else if (grade == "B") {
+//   console.log("Хорошо");
+// } else if (grade == "C") {
+//   console.log("Удовлетворительно");
+// } else if (grade == "D") {
+//   console.log("Неудовлетворительно");
+// } else if (grade == "F") {
+//   console.log("Плохо");
+// } else {
+//   console.log("Неверная оценка");
+// }
